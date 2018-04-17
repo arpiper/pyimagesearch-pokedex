dd4d24c9b3acd506f923f176e6b64e4eaab6fa7b
Andrew Piper <pipera@uw.edu>
2018-04-17 16:13:41 -0700

    tutorial part 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Python Convolutional Neural Network tutorial
 
 > [Tutorial on pyimagesearch.com](https://www.pyimagesearch.com/2018/04/09/how-to-quickly-build-a-deep-learning-image-dataset/)
+> [Part1](https://www.pyimagesearch.com/2018/04/16/keras-and-convolutional-neural-networks-cnns/)
--- pyimagesearch/smallervggnet.py
+++ pyimagesearch/smallervggnet.py
@@ -22,3 +22,44 @@ class SmallerVGGNet:
         if K.image_data_format() == 'channels_first':
             inputShape = (depth, height, width)
             chanDim = 1
+
+        # CONV => RELU => POOL
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(64, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(128, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # First (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation('softmax'))
+
+        # return the contructed network architecture
+        return model
